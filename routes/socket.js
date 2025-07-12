2773686237afc941b24e8959b415d03ac4e68d2d
AdithyaShenal <washenal55@gmail.com>
2025-07-12 15:44:48 +0530

    Bug fixed

--- routes/socket.js
+++ routes/socket.js
@@ -6,7 +6,7 @@ module.exports = function (server) {
 
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://chatapp-client-tawny.vercel.app",
       methods: ["GET", "POST"],
     },
   });
